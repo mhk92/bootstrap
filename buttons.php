3af8c2e5cd7eebaed1afcb34104b7ff968a20c7f
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 12:34:48 +0330

    buttons

--- buttons.php
+++ buttons.php
@@ -25,7 +25,31 @@
 	<a href="#" class="btn btn-secondary">anchor tag with secondary color</a>
 </div>
 
-
+<br><br>
+<div class="justify-content-between flex text-center">
+	<h3>different button size</h3>
+	<button class="btn btn-lg btn-success">large success button</button>
+	<button class="btn btn-sm btn-warning">small success button</button>
+</div>
+<br><br>
+<div class="justify-content-between text-center flex">
+	<h3>button style</h3>
+	<button class="btn btn-outline-secondary btn-lg">outlined button</button>
+	<button class="btn btn-outline-danger btn-lg">outlined button</button>
+	<button class="btn btn-outline-warning btn-sm">outlined button</button>
+</div>
+<br><br>
+<div class="flex text-center justify-content-between">
+	<h3>group buttons</h3>
+	<div class="btn-group ">
+		<a href="#" class="btn btn-outline-primary">number one</a>
+		<a href="#" class="btn btn-outline-secondary">number two</a>
+		<a href="#" class="btn btn-outline-danger">number three</a>
+		<a href="#" class="btn btn-outline-warning">number four</a>
+	</div>
+</div>
+<br><br>
+<br><br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
