379b18724af4191f73ce9a969e9789ff6d808109
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 18:20:10 +0330

    grid system

---
+++ gridSystem.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+	<title>mehrnaz</title>
+</head>
+<body>
+<div class="container-lg my-5 p-3 bg-black rounded">
+	<h2>basic grid</h2>
+	<div class="row">
+		<div class="col">
+			<p class="col bg-dark text-light p-4">mehrnaz</p>
+		</div>
+		<div class="col">
+			<p class="col bg-dark text-light p-4">mehrnaz</p>
+		</div>
+		<div class="col">
+			<p class="col bg-dark text-light p-4">mehrnaz</p>
+		</div>
+		<div class="col">
+			<p class="col bg-dark text-light p-4">mehrnaz</p>
+		</div>
+	</div>
+	
+	
+</div>
+<hr>
+	<div class="container-lg my-5 p-3 bg-success rounded">
+	<h2>column widths</h2>
+		<div class="row">
+			<div class="col-4">
+				<p class="bg-dark text-light p-4">mehrnaz</p>
+			</div>
+			<div class="col-4">
+				<p class="bg-dark text-light p-4">mehrnaz</p>
+			</div>
+			<div class="col-4">
+				<p class="bg-dark text-light p-4">mehrnaz</p>
+			</div>
+		</div>
+</div>
+<hr>
+<div class="container-lg my-5 p-3 bg-secondary text-white rounded">
+	<h2>responsive column widths</h2>
+	<div class="row">
+		<div class="col-sm-6 col-lg-4">
+			<p class="bg-dark text-light p-4">mehrnaz</p>
+		</div>
+		<div class="col-sm-3 col-lg-4">
+			<p class="bg-dark text-light p-4">mehrnaz</p>
+		</div>
+		<div class="col-sm-3 col-lg-4">
+			<p class="bg-dark text-light p-4">mehrnaz</p>
+		</div>
+	</div>
+	
+</div>
+<hr>
+<div class="container-lg my-5 p-3 bg-secondary text-white rounded">
+	<h2>justifying columns</h2>
+	<div class="row justify-content-between">
+		<div class="col-md-3">
+			<div class="p-4 bg-success text-light">
+				mehrnaz
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="p-4 bg-success text-light">
+				mehrnaz
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="p-4 bg-success text-light">
+				mehrnaz
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
