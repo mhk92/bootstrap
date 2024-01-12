44d6bbfd968c71f06e63361cba15fea8a91d150e
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 18:59:13 +0330

    grid project part one

---
+++ prject.grid.php
@@ -0,0 +1,53 @@
+<html lang="">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+	<title>project part</title>
+	<style>
+       section {
+           padding: 60px 0;
+       }
+	</style>
+</head>
+<body>
+<!-- navbar -->
+
+<!-- main image and intro text -->
+
+<section id="intro">
+	<div class="container-lg">
+		<div class="row justify-content-center align-items-center">
+			<div class="col-md-5 text-center text-md-start">
+				<h1>
+					<div class="display-2">black belt</div>
+					<div class="display-5 text-muted">your coding skills</div>
+				</h1>
+				<p class="lead my-4 text-muted">Lorem ipsum dolor sit amet.</p>
+				<a href="#pricing" class="btn btn-secondary btn-large">buy now</a>
+			</div>
+			<div class="col-md-5 text-center d-none d-md-block">
+				<img class="img-fluid" src="/assets/hp_camera_photo.ico" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- pricing plans -->
+
+
+<!-- topic at a glance -->
+
+
+<!-- reviews list -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
