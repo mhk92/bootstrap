f7ac70efea3070c9e99e6210d999e3c3707af01b
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 16:22:51 +0330

    contaner class

---
+++ container.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>mehrnaz</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+</head>
+<body>
+<div class="container my-3 justify-content-center bg-dark text-white">
+	<h2>normal container</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+		elit. Aperiam at eos facilis impedit nisi nulla praesentium
+		quibusdam? Dolorum ea eveniet, fugiat iusto minima sit? Animi,
+		at autem corporis distinctio earum, eius, exercitationem hic incidunt
+		iste laboriosam maiores molestias mollitia nulla quae quaerat quas ullam
+		ut veniam vero voluptatem. Quibusdam, soluta.</p>
+</div>
+<div class="container my-3 justify-content-center text-white bg-dark">
+	<h2>large container</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+		elit. Aliquam animi atque distinctio eveniet expedita
+		modi placeat quo reiciendis sequi sunt! At consequuntur
+		, culpa hic ipsa labore magnam maiores mollitia nobis,
+		odio optio perspiciatis quas sunt ut, vel veritatis. Aperiam
+		est possimus quo quos sequi! Ab aliquid aperiam architecto autem
+		, beatae commodi dolor ducimus eius enim exercitationem, inventore
+		itaque iure mollitia nihil officia omnis pariatur perspiciatis quae
+		quaerat quasi sunt unde velit. Atque et expedita minus neque quam quibusdam
+		reiciendis sit.</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
