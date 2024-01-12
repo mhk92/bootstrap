3f1f0a9d7461cc285e0c80384ca1e679aa55e62f
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 11:50:54 +0330

    text color and weight

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>mehrnaz</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+
+</head>
+<body>
+
+<p class="text-primary text-center">Lorem ipsum dolor.</p>
+<p class="text-secondary text-center">Lorem ipsum dolor.</p>
+<p class="text-info text-center">Lorem ipsum dolor.</p>
+<p class="text-danger text-center">Lorem ipsum dolor.</p>
+<p class="text-success text-center">Lorem ipsum dolor.</p>
+<p class="text-warning text-center">Lorem ipsum dolor.</p>
+<p class="text-white bg-primary text-center">Lorem ipsum dolor.</p>
+<p class="text-white text-center bg-secondary">Lorem ipsum dolor.</p>
+<p class="text-gray text-center bg-danger">Lorem ipsum dolor.</p>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
