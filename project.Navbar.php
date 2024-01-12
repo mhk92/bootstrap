3ef4353a4ca0faa422cc0a0d5b2a63747b7aea70
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 21:43:50 +0330

    navbar again

--- project.Navbar.php
+++ project.Navbar.php
@@ -1,1 +1,87 @@
-<?php
+<html lang="">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+	<title>project part</title>
+	<style>
+       section {
+           padding: 60px 0;
+       }
+	</style>
+</head>
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-secondary">
+	<div class="container-xxl">
+		<a href="#intro" class="navbar-brand">
+			<span class="fw-bold text-warning">
+				something about the project
+			</span>
+		</a>
+		<!-- toggle button for mobile nav-->
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+			data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false"
+			     aria-label="Toggle-navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<!-- navbar links-->
+		<div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="#topics" class="nav-link">about the project</a>
+				</li>
+				<li class="nav-item">
+					<a href="#review" class="nav-link">reviews</a>
+				</li>
+				<li class="nav-item">
+					<a href="#contact" class="nav-link">get in touch</a>
+				</li>
+				<li class="nav-item d-md-none">
+					<a href="#pricing" class="nav-link">pricing</a>
+				</li>
+				<li class="nav-item ms-2 d-none d-md-inline">
+					<a href="#pricing" class="btn btn-primary">buy now</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<!-- main image and intro text -->
+
+<section id="intro">
+	<div class="container-lg">
+		<div class="row justify-content-center align-items-center">
+			<div class="col-md-5 text-center text-md-start">
+				<h1>
+					<div class="display-2">black belt</div>
+					<div class="display-5 text-muted">your coding skills</div>
+				</h1>
+				<p class="lead my-4 text-muted">Lorem ipsum dolor sit amet.</p>
+				<a href="#pricing" class="btn btn-secondary btn-large">buy now</a>
+			</div>
+			<div class="col-md-5 text-center d-none d-md-block">
+				<img class="img-fluid" src="/assets/hp_camera_photo.ico" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- pricing plans -->
+
+
+<!-- topic at a glance -->
+
+
+<!-- reviews list -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
