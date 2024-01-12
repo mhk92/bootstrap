3ec8675e2f281082423b4f07e5278a32072a2d22
mohammadhosein kamari <mohammadhosein.kamari@gmail.com>
2024-01-12 14:05:52 +0330

    utility classes

---
+++ utility.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>mehrnaz</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+	      rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+	      crossorigin="anonymous">
+</head>
+<body>
+<h1 class="text-center">utility classes</h1>
+<hr>
+<!-- margin and paddings -->
+<h3 class="text-center">margin and paddings</h3>
+<div class="bg-black text-white-50 m-2 p-2 text-center flex justify-content-center">
+	small margin and padding
+</div>
+<hr>
+<!-- borders -->
+<h3 class="text-center">borders</h3>
+<div class="text-center flex justify-content-center fw-bold m-3 p-3
+            border-top border-bottom border-start border-end rounded border border-2 ">
+	using border class
+</div>
+<div class="text-center flex justify-content-center fw-bold m-3 p-3
+             border rounded-pill border-5 border-opacity-10">
+	pill border
+</div>
+<hr>
+<!-- box shadows -->
+<h3 class="text-center">box shadows</h3>
+<div class="text-center justify-content-center fw-bold m-3 p-3
+				shadow-sm">
+	elements with small box shadow
+</div>
+<div class="text-center justify-content-center fw-bold m-3 p-3
+				shadow-lg">
+	elements with large box shadow
+</div>
+
+<hr>
+<!-- font weight -->
+<h3 class="text-center">font weight</h3>
+<div class="text-center justify-content-center border-5 shadow-sm m-3 p-3">
+	<p class="fw-bold">bold font weight</p>
+	<p class="fw-bolder">bolder font weight</p>
+	<p class="fw-light">light font weight</p>
+	<p class="fw-lighter">lighter font weight</p>
+	<p class="fst-italic fw-light">light font weight with italic style</p>
+</div>
+<hr>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
